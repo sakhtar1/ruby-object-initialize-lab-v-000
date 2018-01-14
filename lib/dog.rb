80fe03181c4cd068dabd1ea9ecafd495700c4f71
Sharmina Akhtar <sakht318@gmail.com>
2018-01-14 09:48:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,20 @@ class Dog
     @breed = breed
     breed = "Mutt"
   end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=("Mutt")
+    @breed = "Mutt"
+  end
+
+  def breed
+    @breed
+  end
 end
